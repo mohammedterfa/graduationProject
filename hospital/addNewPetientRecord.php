223f1e510cf4d3378887e56e621334084fb22543
mohammedterfa <mohammedterfa@gmail.com>
2021-09-22 23:32:21 +0200

    new lines of code added

--- hospital/addNewPetientRecord.php
+++ hospital/addNewPetientRecord.php
@@ -20,7 +20,7 @@
         <div class="admin-navbar">
             <div class="left">
                 <ul>
-                <li><a href="admin.php"><i class="fas fa-home"></i></a></li>
+                <li><a href="hospital.php"><i class="fas fa-home"></i></a></li>
                 <li><a href="#"><i class="fas fa-envelope"></i></a></li>
                 <li><a href="#"><i class="fas fa-bell"></i></a></li>
                 </ul>
@@ -88,97 +88,48 @@
                 <input type="text"  required name="address">
                 <div class="placeholder">عنوان سكن المريض</div>
             </div>
+
+            <div class="textField">
+                <input type="date"  required name="address">
+                <div class="placeholder">تاريخ الميلاد</div>
+            </div>
             
             <div class="select" >
                 <select name="state">
-                <option value="">العمر</option>
-                <option value="1">1</option>
-                <option value="2">2</option>
-                <option value="3">3</option>
-                <option value="4">4</option>
-                <option value="5">5</option>
-                <option value="6">6</option>
-                <option value="7">7</option>
-                <option value="8">8</option>
-                <option value="9">9</option>
-                <option value="10">10</option>
-                <option value="11">11</option>
-                <option value="12">12</option>
-                <option value="13">13</option>
-                <option value="14">14</option>
-                <option value="15">15</option>
-                <option value="16">16</option>
-                <option value="17">17</option>
-                <option value="18">18</option>
-                <option value="19">19</option>
-                <option value="20">20</option>
-                <option value="21">21</option>
-                <option value="22">22</option>
-                <option value="23">23</option>
-                <option value="24">24</option>
-                <option value="25">25</option>
-                <option value="26">26</option>
-                <option value="27">27</option>
-                <option value="28">28</option>
-                <option value="29">29</option>
-                <option value="30">30</option>
-                <option value="31">31</option>
-                <option value="32">32</option>
-                <option value="33">33</option>
-                <option value="34">34</option>
-                <option value="35">35</option>
-                <option value="36">36</option>
-                <option value="37">37</option>
-                <option value="38">38</option>
-                <option value="39">39</option>
-                <option value="40">40</option>
-                <option value="41">41</option>
-                <option value="42">42</option>
-                <option value="43">43</option>
-                <option value="44">44</option>
-                <option value="45">45</option>
-                <option value="46">46</option>
-                <option value="47">47</option>
-                <option value="48">48</option>
-                <option value="49">49</option>
-                <option value="50">50</option>
-                <option value="51">51</option>
-                <option value="52">52</option>
-                <option value="53">53</option>
-                <option value="54">54</option>
-                <option value="55">55</option>
-                <option value="56">56</option>
-                <option value="57">57</option>
-                <option value="58">58</option>
-                <option value="59">59</option>
-                <option value="60">60</option>
-                <option value="61">61</option>
-                <option value="62">62</option>
-                <option value="63">63</option>
-                <option value="64">64</option>
-                <option value="65">65</option>
-                <option value="66">66</option>
-                <option value="67">67</option>
-                <option value="68">68</option>
-                <option value="69">69</option>
-                <option value="70">70</option>
-                <option value="71">71</option>
-                <option value="72">72</option>
-                <option value="73">73</option>
-                <option value="74">74</option>
-                <option value="75">75</option>
-                <option value="76">76</option>
-                <option value="77">77</option>
-                <option value="78">78</option>
-                <option value="79">79</option>
-                <option value="80">80</option>
-                <option value="+80">+80</option>
+                <option value="">فصيلة الدم</option>
+                <option value="+A">+A</option>
+                <option value="+B">+B</option>
+                <option value="+AB">+AB</option>
+                <option value="+O">+O</option>
+                <option value="-A">-A</option>
+                <option value="-B">-B</option>
+                <option value="-AB">-AB</option>
+                <option value="-O">-O</option>
 
                
                 </select >        
                
             </div>
 
+            <div class="textb">
+                <input type="text" required name="phone">
+                <div class="placeholder">المرض/ الأمراض</div>
+            </div>
+
+            <div class="textb">
+                <input type="text" required name="phone">
+                <div class="placeholder">الدواء / الأدوية</div>
+            </div>
+
+            <div class="textb">
+                <input type="text" required name="phone">
+                <div class="placeholder">الأدوية التي تسبب حساسية للمريض أو الممنوعة</div>
+            </div>
+
+            <div class="textb">
+                <input type="file" required name="phone">
+                <div class="placeholder">(pdf)نتائج الاختبارات المعملية</div>
+            </div>
             
             <div class="textb">
                 <input type="text" required name="phone">
@@ -189,19 +140,6 @@
                 <input type="text" required name="phone">
                 <div class="placeholder">رقم هاتف بديل</div>
             </div>
-
-            <div class="textb">
-                <input type="text" required name="username">
-                <div class="placeholder">اسم المستخدم</div>
-            </div>
-
-            <div class="textb">
-                <input type="text" required name="password">
-                <div class="placeholder">كلمة المرور</div>
-            </div>
-            
-
-            
 
             <center><button class="btnnew"  name="addnewhospital" type="submit">إضافة مستشفى جديدة</button>
             
